d11b0cf9e81d488cccc948927ea6615d8a6d9c2e
Fabiana Tavares <fabi.tavares1@gmail.com>
2018-04-12 19:11:05 -0300

    output property

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import {CursosModule} from './cursos/cursos.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiro2Component,
-    DataBindingComponent
+    DataBindingComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule,
-    CursosModule
+    CursosModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,43 @@ url: string = 'http://loiane.com';
 cursoAngular: boolean = true;
 urlImagem = "http://prmurphy.com.au/wp-content/uploads/2015/10/pexels-photo-236047.jpg";
 
+ValorAtual: string = '';
+ValorSalvo = '';
+
+isMouseOver: boolean = false;
+
+nome: string = 'abc';
+
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial = 15;
+
 getValor() {
   return 1;
 }
 
 getCurtirCurso(){
   return true;
+}
+
+botaoClicado(){
+  alert('Botao Clicado')
+}
+
+onKeyUp(evento: KeyboardEvent){
+  this.ValorAtual = (<HTMLInputElement>evento.target).value;
+}
+
+salvarValor(valor){
+  this.ValorSalvo = valor;
+}
+
+onMouseOverOut(){
+  this.isMouseOver = !this.isMouseOver;
+}
+
+onMudouValor(evento){
+  console.log(evento.novoValor);
 }
 
   constructor() { }
