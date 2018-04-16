caa07ca2d9ff5468cbfba066eaefe9e89fb3720b
Fabiana Tavares <fabi.tavares1@gmail.com>
2018-04-16 13:15:33 -0300

    53 e 54 parametros de rotas e escutando mudanças nos parametros.

---
+++ exemplo-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent implements OnInit {
+
+  id: string;
+  inscricao: Subscription;
+
+  constructor(private route: ActivatedRoute) { 
+    //this.id = this.route.snapshot.params['id'];
+    
+  }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe( // uso de aero functions do es6
+      (params: any) =>{
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+}
