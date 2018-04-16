5eac6aae74ad300eca47a51b78d1ef8a7b7c0559
Fabiana Tavares <fabi.tavares1@gmail.com>
2018-04-16 10:21:44 -0300

    aplicando pipes de filtros corretamente

--- curso-angular-directivas/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ curso-angular-directivas/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -16,6 +16,27 @@ export class ExemplosPipesComponent implements OnInit {
     url: 'http://loiane.training'
   } 
 
+  livros: string[] = ['Java', 'Angular 2'];
+  filtro: string;
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos(){
+    if(this.livros.length === 0 || this.filtro === undefined || this.filtro.trim() === ''){
+      return this.livros;
+    }
+
+    return this.livros.filter((v) => {
+      if(v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0){
+        return true;
+      }
+      return false;
+    });
+  }
+
   constructor() { }
 
   ngOnInit() {
