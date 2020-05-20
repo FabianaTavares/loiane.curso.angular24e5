33a920c0de44b7e5cd9ddcaf7c83023aef5f5e55
Fabiana Tavares <fabi.tavares1@gmail.com>
2020-05-20 10:15:17 -0300

    aulas 126 aproximadamente, criação de mensagem de alerta e modal do bootrap 4 de maneira generica

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,11 @@
+import { AlertModalService } from './../../shared/services/alert-modal.service';
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../models/curso';
 import { CursosService } from '../services/cursos.service';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -11,14 +16,52 @@ import { CursosService } from '../services/cursos.service';
 export class CursosListaComponent implements OnInit {
 
   cursos: Curso[];
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
+  //bsModalRef: BsModalRef;
 
   constructor(
-    private service: CursosService
+    private service: CursosService,
+    private alertService: AlertModalService
+
   ) { }
 
   ngOnInit() {
+    /*  this.service.list()
+     .subscribe(dados => this.cursos = dados); */
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list()
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          // this.error$.next(true);
+          this.handleError();
+          // tslint:disable-next-line: deprecation
+          return empty();
+        })
+      );
+
     this.service.list()
-    .subscribe(dados => this.cursos = dados);
+      .pipe(
+        // tslint:disable-next-line: deprecation
+        catchError(error => empty())
+      )
+      .subscribe(
+        dados => {
+          console.log(dados);
+        }
+      );
+  }
+
+  handleError() {
+    /* this.bsModalRef = this.modalService.show(AlertModalComponent);
+    this.bsModalRef.content.type = 'danger';
+    this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
+  */
+    this.alertService.showAlertDanger('Erro ao carregar cursos. Tente novamente mais tarde.');
   }
 
 }
--- requests-http/src/app/cursos/services/cursos.service.ts
+++ requests-http/src/app/cursos/services/cursos.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap, delay } from 'rxjs/operators';
 import { Curso } from '../models/curso';
-import { tap } from 'rxjs/operators';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(
     private http: HttpClient,
@@ -17,6 +18,7 @@ export class CursosService {
   list() {
     return this.http.get<Curso[]>(this.API)
       .pipe(
+        delay(2000),
         tap(console.log)
       );
   }
